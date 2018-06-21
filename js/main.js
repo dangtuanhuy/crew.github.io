9f49cebaaeeab0d997ed9fc13296284f2bf3cfec
lmint3010 <jeonkyosin@gmail.com>
2018-06-21 18:49:17 +0700

    Complete set color for box Code
    Hoàn thiện hàm set màu cho code trong Box Code
    
    Co-Authored-By: poxy27 <poxy27@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -17,78 +17,102 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(getCodeTag[1]);
     for (var gr = 0; gr < getCodeTag.length; gr++) {
         var getText = getCodeTag[gr].innerHTML; getText.trim();
-        var tagTemp = /^&lt;/;
+        var stt, cache= new String();
         // Nếu bắt đầu bằng <
-        if (tagTemp.test(getText)) {
-            // Tạo bộ nhớ đệm cache
-            var cache = new String(), stt;
-            if(getText[4]!="!") {
-                cache+='<span class="codetag">'; stt = 1;
+        if (getText.startsWith('&lt;')) {
+            if(getText.startsWith('&lt;!--')) {
+                stt=5; cache+=`<span class="codecmt">`;
             } else {
-                cache+='<span class="codecmt">'; stt = 5;
-            }
-            // Duyệt chuỗi từ đầu
-            for (var n = 0; n < getText.length; n++) {
-                if (stt == 1) {
-                    if (getText[n] == '&' && getText[n+4] == '!') {
-                        cache += `<span class="codecmt">${getText[n]}`;
-                        stt = 5;
-                    }
-                    // Nếu là space
-                    if (getText[n] == ' ') {
-                        cache += `</span><span class="codeatt">`;
-                        stt = 2;
-                    } else {
-                        // Không phải space
-                        cache += `${getText[n]}`;
-                    };
-                    if (getText[n] == '&' && getText[n + 1] == 'g' && getText[n + 3] == ';') {
-                        cache += `gt;`;
-                        n+=3; 
-                        // Nếu tiếp theo không phải <
-                        if(getText[n+1] != '&' && getText[n+2] != 'l' && getText[n+4] != ';') {
-                            stt=4; cache+='</span>';
-                            if(n!=getText.length-1) {
-                                cache+=`<span class="codenor" stt1>`;
-                            };
+                stt=1; cache+=`<span class="codetag">`;
+            };
+        }
+        else if (getText.startsWith('&lt;!--')) {stt=5; cache+=`<span class="codecmt">`;};
+
+        // Duyệt chuỗi từ đầu
+        for (var n = 0; n < getText.length; n++) {
+            if (stt == 1) {
+                // Nếu là space
+                if (getText[n] == ' ') {
+                    cache += `</span><span class="codeatt">`;
+                    stt = 2;
+                } else {
+                    // Không phải space
+                    cache += `${getText[n]}`;
+                };
+                if (getText[n] == '&' && getText[n + 1] == 'g' && getText[n + 3] == ';') {
+                    cache += `gt;`;
+                    n+=3; 
+                    // Nếu tiếp theo không phải <
+                    if(getText[n+1] != '&' && getText[n+2] != 'l' && getText[n+4] != ';') {
+                        stt=4; cache+='</span>';
+                        if(n!=getText.length-1) {
+                            cache+=`<span class="codenor">`;
                         };
-                    }
-                };
-                // Attribute color
-                if (stt == 2) {
-                    if (getText[n] == '=') {
-                        cache += `${getText[n]}</span><span class="codein">`;
-                        stt = 3;
-                    } else {
-                        cache += `${getText[n]}`;
                     };
-                };
-                if(stt==3) {
-                    if(getText[n+1]!='"') {
-                        cache+=getText[n];
-                    } else {
-                        if(getText[n+2]==' ') {
-                            cache+=`${getText[n]}"</span><span class="codeatt">`; stt=2; n++;
-                        }
-                        if(getText[n+2]=='&' && getText[n+3]=='g' && getText[n+5]==';') {
-                            cache+=`${getText[n]}"</span><span class="codetag">`; stt=1; n++;
-                        }
-                    };
-                };
-                if(stt==4) {
-                    // Nếu hiện tại là <
-                    if (getText[n] == '&' && getText[n + 1] == 'l' && getText[n + 3] == ';') {
-                        cache+=`</span><span class="codetag" stt4-1>${getText[n]}`;
-                        stt=1;
-                    } else {
-                        if(getText[n]!=';') {
-                            cache+=`${getText[n]}`; 
-                        }               
-                    };
+                }
+            };
+            // Attribute color
+            if (stt == 2) {
+                if (getText[n] == '=') {
+                    cache += `${getText[n]}</span><span class="codein">`;
+                    stt = 3;
+                } else {
+                    cache += `${getText[n]}`;
                 };
             };
+            if(stt==3) {
+                if(getText[n+1]!='"') {
+                    cache+=getText[n];
+                } else {
+                    if(getText[n+2]==' ') {
+                        cache+=`${getText[n]}"</span><span class="codeatt">`; stt=2; n++;
+                    }
+                    if(getText[n+2]=='&' && getText[n+3]=='g' && getText[n+5]==';') {
+                        cache+=`${getText[n]}"</span><span class="codetag">`; stt=1; n++;
+                    }
+                };
+            };
+            if(stt==4) {
+                // Nếu hiện tại là <
+                if (getText[n] == '&' && getText[n + 1] == 'l' && getText[n + 3] == ';') {
+                    if(getText[n+4]=='!' && getText[n+5]=="-" &&getText[n+6]=='-') {
+                        cache+=`</span><span class="codecmt">`;
+                        stt=5;
+                    } else {
+                        cache+=`</span><span class="codetag">${getText[n]}`;
+                        stt=1;
+                    }
+                } 
+                else {
+                    if(getText[n]!=';') {
+                        cache+=`${getText[n]}`; 
+                    }               
+                };
+            };
+            if(stt==5) {
+                // Kiểm tra xem đến ngoặc đóng chưa
+                if (getText[n] == '&' && getText[n + 1] == 'g' && getText[n + 3] == ';') {
+                    cache += `&gt;`;
+                    n+=4;
+                    // Nếu hiện tại là <
+                    if (getText[n] == '&' && getText[n + 1] == 'l' && getText[n + 3] == ';') {
+                        if(getText[n+4]=='!' && getText[n+5]=="-" &&getText[n+6]=='-') {
+                            cache+=`</span><span class="codecmt">`;
+                            stt=5;
+                        } else {
+                            cache+=`</span><span class="codetag">${getText[n]}`;
+                            stt=1;
+                        }
+                    } 
+                    else {
+                        cache+=`</span><span class="codenor">${getText[n]}`;
+                        stt=4;              
+                    };
+                } else {
+                    cache+=getText[n];
+                }
+            }
         };
-        console.log(cache);
         getCodeTag[gr].innerHTML = cache;
     };
     // End Code Box
